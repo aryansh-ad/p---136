58d367a581c63e7665b26044f4271a86d52a77e8
aryansh-ad <85281389+aryansh-ad@users.noreply.github.com>
2023-01-13 15:51:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+video = "";
+status = "";
+
+function setup() {
+    canvas = createCanvas(480,380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+}
+function start() {
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById(status),innerHTML ="status : Detecting Objects";
+    
+}
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
+function draw(){
+    image(video,0,0,480,380)
+}
